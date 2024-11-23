f6d90e606c1e526bb7d814257e2dfd1b15183dc6
Brad Heller <brad@tower.dev>
2024-11-23 09:55:49 +0100

    fix: Expand env vars for the user

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,6 +6,10 @@ dlt pipeline -l
 dlt pipeline --profile=dev linear run
 dlt dataset linear info
 ls ./_storage
+
+# expand the ~ to the home directory when it wasn't passed in properly.
+LOCAL_STORAGE_VOLUME="${LOCAL_STORAGE_VOLUME/#\~/$HOME}"
+
 if [ -d "./_storage" ]; then
   echo "Copying data from ./_storage to $LOCAL_STORAGE_VOLUME"
   mkdir -p $LOCAL_STORAGE_VOLUME
